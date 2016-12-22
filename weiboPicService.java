b3d59020adfa2faf3693ed49336dc9c7cf027fc0
14301100 <Water_b@outlook.com>
2016-12-22 10:51:05 +0800

    Update weiboPicService.java

--- weiboPicService.java
+++ weiboPicService.java
@@ -1,35 +1,49 @@
 package cn.edu.bjtu.weibo.service;
 
 import java.io.File;
+import java.util.LinkedList;
 import java.util.List;
 
 import cn.edu.bjtu.weibo.dao.WeiboDAO;
+import cn.edu.bjtu.weibo.model.Picture;
 import cn.edu.bjtu.weibo.model.Weibo;
 
-public class weiboPicService implements WeiboListService{
+public class weiboPicService implements WeiboPictureServie{
 	
 	@Override
 	public boolean uploadWeiboPicture(String weiboId,
 			MultipartFile multipartFile) {
-		boolean fig;//ÅĞ¶ÏÊÇ·ñÉÏ´«³É¹¦
+		boolean fig;//åˆ¤æ–­æ˜¯å¦ä¸Šä¼ æˆåŠŸ
 		 WeiboDAO weiboDao = new WeiboDAO();
-		// ´´½¨±£´æµÄÎÄ¼şµÄÖ¸¶¨Â·¾¶
+		// åˆ›å»ºä¿å­˜çš„æ–‡ä»¶çš„æŒ‡å®šè·¯å¾„
 		String path = "C:\\Users\\liu\\Desktop\\map_12_21_1\\img";
-		// »ñÈ¡¸ÃÎÄ¼şµÄÎÄ¼şÃû
+		// è·å–è¯¥æ–‡ä»¶çš„æ–‡ä»¶å
 		String fileName = multipartFile.getOriginalFilename();
 		File targetFile = new File(path, fileName);
 		String url=path+"\\"+fileName;
 		if (!targetFile.exists()) {
 			targetFile.mkdirs();
 		}
-		// ±£´æ
+		// ä¿å­˜
 		try {
 			multipartFile.transferTo(targetFile);
-			//´æ´¢url
+			//å­˜å‚¨url
 			fig = weiboDao.insertWeiboPicture(weiboId,url); 
 			return fig;
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	@Override
+	public List<Picture> getWeiboPictureList(String weiboId) {
+		WeiboDAO weiboDao = new WeiboDAO();
+		List<Picture> pic = new LinkedList<Picture>();
+		for(int i = 0;i < weiboDao.getWeiboPicurlOr(weiboId).size();i++) {
+			pic.get(i).setPicurl(weiboDao.getWeiboPicurlOr(weiboId).get(i));
+		}
+		return pic;
+ }
+
+	
 }
